a6629d3e3e1f4ba3b6d35f89d3f601f2071d4755
pawel-laptop <p.blaszczy@gmail.com>
2023-02-06 19:07:12 +0100

    Main displays a simple list
    As I wanted to see how it works :)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "Canvas.h"
 #include "Frame.h"
+#include "ListWindow.h"
+#include "Stock.h"
+#include "Keys.h"
 
 int main()
 {
@@ -11,6 +14,19 @@ int main()
     f0.draw(c, 7, 2);
     f0.draw(c, 76, 22);
     f1.draw(c, 8, 10);
+
+    ListWindow<Stock> lw(20, 10, 'x', ' ');
+    lw.add(Stock("Item A"));
+    lw.add(Stock("Item B"));
+    lw.add(Stock("Item C"));
+    lw.add(Stock("Item D1"));
+    lw.add(Stock("Item D2"));
+    lw.add(Stock("Item E"));
+
+    lw.navigate(TKey::KEY_DOWN);
+    lw.navigate(TKey::KEY_DOWN);
+
+    lw.draw(c, 40, 5);
 
     c.render();
     return 0;
