2e0ea3bd782ffe0ae7217c1500b4043795720d59
pawel-laptop <p.blaszczy@gmail.com>
2023-02-13 17:45:28 +0100

    Override Baner draw as I want to store also location

--- InputWindow.cpp
+++ InputWindow.cpp
@@ -1,6 +1,17 @@
 #include "InputWindow.h"
 #include <string>
+#include <ncurses.h>
 
 using namespace std;
 
+void InputWindow::draw(Canvas &c, int x, int y)
+{
+    this->x = x;
+    this->y = y;
+    Baner::draw(c, x, y);
+}
 
+int InputWindow::getInput(void)
+{
+    //
+}
--- InputWindow.h
+++ InputWindow.h
@@ -4,15 +4,18 @@
 
 class InputWindow: public Baner
 {
+    protected:
     enum {
         ROOM_FOR_INPUT = 6
     };
+    int x = 0;
+    int y = 0;
 
     public:
     InputWindow(std::string msg, char border='X'): 
         Baner(msg+std::string(ROOM_FOR_INPUT,' '), border) {};
     virtual ~InputWindow() {};
-
-    int getInput(void) {return 42;};
+    void draw(Canvas &c, int x, int y) override;
+    int getInput(void);
 };
 
