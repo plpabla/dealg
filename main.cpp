8d2dfdb5f6d0904c2d4cf3521770545adbdd5440
pawel-laptop <p.blaszczy@gmail.com>
2023-02-17 09:34:25 +0100

    add color scheme

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ using namespace std;
 int main()
 {
     initscr();
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    attron(COLOR_PAIR(1));
+
     raw();
     keypad(stdscr, TRUE);
     noecho();
