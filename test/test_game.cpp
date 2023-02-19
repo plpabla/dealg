37e81e59d6dbfd3a12245165cdffd59ef2a0d5ed
pawel-laptop <p.blaszczy@gmail.com>
2023-02-19 15:55:29 +0100

    Create TestGame class to access private Game members

--- test/test_game.cpp
+++ test/test_game.cpp
@@ -25,3 +25,23 @@ TEST(GameTest, CanUpdateBudget)
 
     ASSERT_EQ(g.getBudget(), 100);
 }
+
+class TestGame: public Game
+{
+    public:
+    ListWindow<Stock>* create_assets_list(void) {return Game::create_assets_list();};
+};
+
+TEST(GameTest, CanAccessAssetsListUsingChildClass)
+{
+    TestGame g;
+
+    ListWindow<Stock>*p = g.create_assets_list();
+
+    ASSERT_NE(p->getNumberOfElements(), 0);
+}
+
+TEST(GameTest, CanInitWithCustomFiles)
+{
+    ASSERT_NO_THROW(Game g(10000, "assets.txt", "cities.txt"));
+}
